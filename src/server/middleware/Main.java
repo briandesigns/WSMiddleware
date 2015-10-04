3913e29c5e6ba56bf3b01d9f3422b45980edcd7a
brian zee <brian.zhou@mail.mcgill.ca>
2015-10-04 13:00:10 -0400

    imported over MiddlewarImpl and middleware.Main

---
+++ src/server/middleware/Main.java
@@ -0,0 +1,32 @@
+package server.middleware;
+
+import org.apache.catalina.startup.Tomcat;
+import java.io.File;
+import org.apache.catalina.LifecycleException;
+
+/**
+ * embedded tomcat launcher that deploys the middleware
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        if (args.length != 3) {
+            System.out.println("Usage: java Main <service-name> <service-port> <deploy-dir>");
+            System.exit(-1);
+        }
+        String serviceName = args[0];
+        int port = Integer.parseInt(args[1]);
+        String deployDir = args[2];
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(port);
+        tomcat.setBaseDir(deployDir);
+        tomcat.getHost().setAppBase(deployDir);
+        tomcat.getHost().setDeployOnStartup(true);
+        tomcat.getHost().setAutoDeploy(true);
+        //tomcat.addWebapp("", new File(deployDir).getAbsolutePath());
+        tomcat.addWebapp("/" + serviceName, new File(deployDir + "/" + serviceName).getAbsolutePath());
+        tomcat.start();
+        tomcat.getServer().await();
+    }
+
+}
